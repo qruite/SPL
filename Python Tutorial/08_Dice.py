36db82353f2021b80fa974d47f4c3ecf57d6b0e1
maksim <maksimfhmad@gmail.com>
2024-02-26 10:30:07 +0100

    task dice finished

--- Python Tutorial/08_Dice.py
+++ Python Tutorial/08_Dice.py
@@ -1,28 +1,65 @@
 import random
 import sys
 
-def computer():
-    sum = 0
-    rest = 6
+remainingAttempts = 6
+gameState = True
+sumPlayer = 0
+sumComputer = 0
+state = True
 
+
+def getResult():
+    print("Augensumme Spieler: ", sumPlayer)
+    print("Augensumme Computer: ", sumComputer)
+    print("--------------------------")
     
-def player():
-    sum = 0
-    rest = 6
+    if(sumPlayer > sumComputer):
+        print("Spieler hat gewonnen")
+    elif(sumPlayer == sumComputer):
+        print("Unentschieden")
+    else:
+        print("computer hat gewonnen")
+    
+    print("--------------------------")
+    print("")
 
-
-    print()
 
 def start():
-    computer()
-    player()
+    global remainingAttempts, sumPlayer, sumComputer, state
 
-while True:
+    while state:
+        print("remaining attempts:", remainingAttempts)
+        print("--------------------------")
+        print("1. würfeln")
+        print("2. beenden")
+        print("--------------------------")
+
+        n = int(input())
+
+        if remainingAttempts == 1:
+            state = False
+            getResult()
+        elif n == 1:
+            remainingAttempts -= 1
+            playerResult = random.randint(1, 6)
+            print("spieler: ", playerResult)
+            computerResult = random.randint(1, 6)
+            print("computer: ", computerResult)
+            sumPlayer += playerResult
+            sumComputer += computerResult
+        elif n == 2:
+            state = False
+
+while gameState:
     print("willkomen")
     print("--------------------------")
     print("1. starten")
+    print("2. beenden")
+    print("--------------------------")
 
     n = int(input())
 
     if n == 1:
-        def start()
+        start()
+    elif n == 2:
+        gameState = False
